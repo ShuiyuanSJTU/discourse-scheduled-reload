0dcdb6dadd3e85698153e92d6d41d847441f5ca2
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2023-08-12 17:40:15 +0800

    dev: remove unused objects

--- lib/my_plugin_module/engine.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module ::MyPluginModule
-  class Engine < ::Rails::Engine
-    engine_name PLUGIN_NAME
-    isolate_namespace MyPluginModule
-    config.autoload_paths << File.join(config.root, "lib")
-  end
-end
